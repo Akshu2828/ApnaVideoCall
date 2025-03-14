aa00ea06025f83d85aca58b28323f3cd7b1fd447
Akshay Gangurde <agangurde771@gmail.com>
2025-03-14 20:09:16 +0530

    Fixed routing issue for Render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure correct base path for deployment
   build: {
     outDir: "dist",
   },
   server: {
-    port: 8000, // Optional: Ensure correct port for local dev
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
   },
 });
